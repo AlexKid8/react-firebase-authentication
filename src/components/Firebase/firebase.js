7486ba76a0d65f59fe215acd04a0e0efcf02d117
Alexandre <alexkid8@gmail.com>
2020-03-30 01:13:31 +0200

    Firebase Authentication Interface Created

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 const firebaseConfig = {
 	  apiKey: "AIzaSyDDZCFNElr2b5BIQX6ao7M68JdPU-naDco",
@@ -12,8 +13,26 @@ const firebaseConfig = {
 
 class Firebase {
 	constructor() {
-		app.initializeApp(config);
+		app.initializeApp(firebaseConfig);
+
+		this.auth = app.auth();
 	}
+
+	// ** Auth API **
+	
+	doCreateUserWithEmailAndPassword = (email, password) =>
+		this.auth.signInWithEmailAndPassword(email, password);
+
+	doSignInWithEmailAndPassword = (email, password) => 
+		this.auth.signInWithEmailAndPassword(email, password);
+
+	doSignOut = () => this.auth.signOut();
+
+	doPasswordReset = email => 
+		this.auth.sendPasswordResetEmail(email);
+
+	doPasswordUpdate = password => 
+		this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
