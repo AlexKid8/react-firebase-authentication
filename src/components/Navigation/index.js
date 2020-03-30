e4b9c3174b3ae2352d3d20885755cd89a2ea715f
Alexandre <alexkid8@gmail.com>
2020-03-31 00:43:14 +0200

    Auth Implemented In App & Navigation

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
 
-const Navigation = () => (
-	<div>
+const Navigation = ({ authUser }) => (
+	<div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
+);
+
+const NavigationAuth = () => (
 		<ul>
-			<li>
-				<Link to={ROUTES.SIGN_IN}>Sign In</Link>
-			</li>
 			<li>
 				<Link to={ROUTES.LANDING}>Landing</Link>
 			</li>
@@ -26,7 +26,18 @@ const Navigation = () => (
 				<SignOutButton />
 			</li>
 		</ul>
-	</div>
+);
+
+const NavigationNonAuth = () => (
+	<ul>
+			<li>
+				<Link to={ROUTES.SIGN_IN}>Sign In</Link>
+			</li>
+			<li>
+				<Link to={ROUTES.LANDING}>Landing</Link>
+			</li>
+
+	</ul>
 );
 
 export default Navigation;
